310a219b6a9fb5b902ceba8edae9cba7a10899a7
andrasnyarai <andrasnyarai@gmail.com>
2017-12-14 17:53:28 +0100

    finally added file to path, it still not takes parameters as input

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import sys
+
+absolute_path = r"C:\Users\Andras\greenfox\my_todo\nyarai_andras_todo\list.py"
+
 class ToDo(object):
 
     def __init__(self):
         self.my_files = []
-        other_file = open("list.py", "r")
+        other_file = open(str(absolute_path), "r")
         self.lines = other_file.readlines()
         for i in self.lines:
             self.my_files.append(i)
@@ -16,46 +19,62 @@ class ToDo(object):
 
 main = ToDo()
 
-counting = open('list.py','r').readlines()
+counting = open(str(absolute_path),'r').readlines()
 
 program_name = sys.argv[0]
-
 arguments = sys.argv[1:]
-
 count = len(arguments)
 
-commands = ['-l','-r','-a','-c']
+commands = ['-l','-r','-a','-c','-la','-rc']
 
-
-if len(sys.argv) == 1 or set(commands) - set(sys.argv) ==set(commands):
-    print('''Command Line Todo application
+if len(sys.argv) == 1 or set(commands) - set(sys.argv) == set(commands):
+    print('''\n
+    Command Line Todo application
     =============================
 
     Command line arguments:
     -l   Lists all the tasks
     -a   Adds a new task
     -r   Removes an task
-    -c   Completes an task''')
+    -c   Completes an task
+    
+    Optional arguments:
+    -la  lists the unchecked
+         tasks only
+    -rc  removes all checked
+         tasks
+    \n''')
 
 if len(sys.argv) == 2:
     if sys.argv[-1] == '-l':
         print(main)
         if len(main.lines) == 0:
             print('NOPE')
+    if sys.argv[-1] == '-la':
+        for i in range(len(main.my_files)):
+            if main.my_files[i][1] == ' ':
+                print(main.my_files[i])
+        if len(main.lines) == 0:
+            print('NOPE')
+    if sys.argv[-1] == '-rc':
+        infile = open(str(absolute_path),'r').readlines()
+        with open(str(absolute_path),'w') as outfile:
+            for line in infile:
+                if str(line[1]) is 'x':
+                    pass
+                else:
+                    outfile.write(line)
     if sys.argv[-1] == '-a':
         print('no task defined')
     if sys.argv[-1] == '-r':
         print('no index provided')
-
-# #     def complete(self):
-#         self.completed = True
-
-#     def __str__(self):
-#         return ("[x] " if self.completed else "[ ] ") + self.name
+    if sys.argv[-1] == '-c':
+        print('no index provided')
 
 if len(sys.argv) == 3:
+
     if sys.argv[-2] == '-a':
-        with open('list.py', 'a') as file:
+        with open(str(absolute_path), 'a') as file:
             file.writelines('[ ]' + sys.argv[-1] +'\n')
     
     if sys.argv[-2] == '-r':
@@ -63,8 +82,8 @@ if len(sys.argv) == 3:
             if int(sys.argv[-1]) > len(counting):
                 print('out of range')
             if int(sys.argv[-1]) <= len(counting):
-                infile = open('list.py','r').readlines()
-                with open('list.py','w') as outfile:
+                infile = open(str(absolute_path),'r').readlines()
+                with open(str(absolute_path),'w') as outfile:
                     for index,line in enumerate(infile):
                         if index != (int(sys.argv[-1]))-1:
                             outfile.write(line)
@@ -72,13 +91,29 @@ if len(sys.argv) == 3:
             print('write a number')
 
     if sys.argv[-2] == '-c':
-        infile = open('list.py','r').readlines()
-        with open('list.py','w') as outfile:
-            for index,line in enumerate(infile):
-                if index != (int(sys.argv[-1]))-1:
-                    outfile.write(line)
-                if index == (int(sys.argv[-1]))-1:
-                    outfile.write(str(line).replace('[ ]', '[x]'))
+        if sys.argv[-1].isnumeric():
+            if int(sys.argv[-1]) > len(counting):
+                print('out of range')
+            if int(sys.argv[-1]) <= len(counting):
+                infile = open(str(absolute_path),'r').readlines()
+                with open(str(absolute_path),'w') as outfile:
+                    for index,line in enumerate(infile):
+                        if index != (int(sys.argv[-1]))-1:
+                            outfile.write(line)
+                        if index == (int(sys.argv[-1]))-1:
+                            check = '[ ]'
+                            if str(line[1]) is ' ':
+                                outfile.write(str(line).replace('[ ]', '[x]', 1))
+                            else:
+                                outfile.write(str(line))
+        else:
+            print('write a number')
 
+if len(sys.argv) == 4:
+
+    if sys.argv[-3] == '-a':
+        with open(str(absolute_path), 'a') as file:
+            file.writelines('[ ]' + sys.argv[-2] +'\n')
+            file.writelines('[ ]' + sys.argv[-1] +'\n')
 
 
